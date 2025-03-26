1bea88a536611b94366e6985fae079f93667cd5a
Ibra Kaslana <108949711+ibragans666@users.noreply.github.com>
2025-03-27 00:16:09 +0700

    Add files via upload

---
+++ src/app/components/Home3.tsx
@@ -0,0 +1,177 @@
+import React from 'react'
+import styles from './Home.module.css'
+
+const Home3 = () => {
+  return (
+    <div className={`${styles.bgpink} flex`}>
+      <div className='pt-[50px] flex ml-[75px]'>
+        <div>
+          <h1 className='text-[128px] tracking-wider'>Flawless</h1>
+          <div className='flex space-x-[15px] justify-center mt-[-30px]'>
+            <div className='space-y-[30px]'>
+              <img src="/elysia8.jpg" alt="" className='w-[150px] h-[150px] rounded-[20px] border-[3px] z-20 relative'/>
+              <img src="/elyisia12.jpg" alt="" className='w-[150px] h-[150px] rounded-[20px] border-[3px] z-20 relative'/>
+              <img src="/cyrene2.jpg" alt="" className='w-[150px] h-[150px] rounded-[20px] border-[3px] z-20 relative'/>
+              <div className='flex'>
+                <div className='ml-[-34px] mt-[-510px] z-10'>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                  <ul className='flex space-x-[36px] space-y-[36px]'>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                    <li className='border w-[10px] h-[10px] bg-white'></li>
+                  </ul>
+                </div>
+                <div className='flex'>
+                  <ul className='text-[26px] mt-[-12px] ml-[30px] space-y-[-7.4px]'>
+                    <li>F</li>
+                    <li>l</li>
+                    <li>a</li>
+                    <li>w</li>
+                    <li>l</li>
+                    <li>e</li>
+                    <li>s</li>
+                    <li>s</li>
+                  </ul>
+                  <ul className='opacity-50 text-[26px] space-y-[-7.4px] mt-[7px]'>
+                    <li>F</li>
+                    <li>l</li>
+                    <li>a</li>
+                    <li>w</li>
+                    <li>l</li>
+                    <li>e</li>
+                    <li>s</li>
+                    <li>s</li>
+                  </ul>
+                  <ul className='opacity-25 text-[26px] mt-[20px] space-y-[-7.4px]'>
+                    <li>F</li>
+                    <li>l</li>
+                    <li>a</li>
+                    <li>w</li>
+                    <li>l</li>
+                    <li>e</li>
+                    <li>s</li>
+                    <li>s</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img src="/elysia11.jpg" alt="" className='w-[318px] h-[593px] rounded-[20px] border-[3px]'/>
+              <p className='text-[20px] text-center pt-[10px]'>Human . Origin . 13th Herrscher</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className='mt-[-50] justify-end border-[2px] w-0 h-[600px] ml-[-1px]'></div>
+          <ul className='text-[36px] space-y-[-7px] ml-[-10px]'>
+            <li>F</li>
+            <li>l</li>
+            <li>a</li>
+            <li>w</li>
+            <li>l</li>
+            <li>e</li>
+            <li>s</li>
+            <li>s</li>
+          </ul>
+        </div>
+      </div>
+      <div className='basis-3/5 pt-[200px] ml-[10px]'>
+        <div className='border-l-[3px] ml-[10px]'>
+          <h1 className='text-[96px]'>Elysian Realm</h1>
+          <p className='ml-[40px] text-[24px] leading-tight text-justify mr-[120px]'>In a poignant moment, Kevin ultimately takes drastic action and ends Elysia's life to prevent further destruction. Although this decision weighs heavily on him, Kevin holds deep respect and care for Elysia, making this act an emotional burden for him.</p>
+          <div className='ml-[40px] border-l-[3px] mt-[20px]'>
+            <p className='text-[24px] leading-tight text-justify mr-[120px] ml-[20px]'>
+            After her death, Elysia's consciousness lives on in the form of a simulation within the Elysian Realm, a virtual space created to preserve the memories of the Flame-Chasers. There, players can interact with Elysia and gain deeper insights into her sacrifices and personality. Her legacy as a compassionate hero continues to inspire new generations to combat darkness.
+            </p>
+            <img src="/elysia14.jpg" alt="" className='w-[357px] h-[201px] rounded-[20px] border-[3px] ml-[20px] mt-[20px]'/>
+          </div>
+          <div>
+              <img src="/signet.svg" className='ml-[200px] w-[500px] h-[500px] mt-[-500px] opacity-10' alt="" style={{filter: 'brightness(0) saturate(100%) invert(100%)'}}/>
+          </div>
+        </div>
+        <div className='mb-[100px]'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home3
