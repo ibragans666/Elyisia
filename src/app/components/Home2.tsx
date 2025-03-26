88890faaaae6eaf8b1948dd8457de00a90ecfeb1
Ibra Kaslana <108949711+ibragans666@users.noreply.github.com>
2025-03-27 00:42:23 +0700

    Add files via upload

---
+++ src/app/components/Home2.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import styles from './Home.module.css'
+
+const Home2 = () => {
+  return (
+    <div className={`${styles.textpink} flex bg-white space-x-[100px]`}>
+      <div className='basis-1/2 ml-[45px] pr-[30px] border-r-3'>
+        <div className='pr-[-100px] mt-[84px] mb-[-30px]'>
+          <p className='[@media(max-width:400px)]:text-[20px] text-[40px] '>2nd Flamechaser</p>
+          <h1 className='[@media(max-width:400px)]:text-[64px] [@media(max-width:400px)]:mt-0 text-[128px] tracking-widest mt-[-30px] text-left ml-[-10px]'>ELYSIA</h1>
+          <p className='[@media(max-width:400px)]:text-[20px] [@media(max-width:400px)]:mt-0 text-justify text-[24px] leading-tight mt-[-40px]'>Elysia is one of the 13 Flame-Chasers, heroes dedicated to protecting humanity from the threat of Honkai. Known for her compassionate nature and self-sacrifice, Elysia is determined to unite a fractured humanity torn apart by war. She possesses extraordinary psychic abilities and often collaborates with her comrades to overcome the various challenges they face.</p>
+        </div>
+        <div className='flex mt-[70px] z-10'>
+          <div className='flex [@media(max-width:400px)]:text-[24px] [@media(max-width:400px)]:space-x-[20px] basis-1/3 text-[48px] space-x-[40px]'>
+            <ul>
+              <li>T</li>
+              <li className='mt-[-15px]'>R</li>
+              <li className='mt-[-15px]'>U</li>
+              <li className='mt-[-15px]'>E</li>
+            </ul>
+            <ul>
+              <li>H</li>
+              <li className='mt-[-15px]'>E</li>
+              <li className='mt-[-15px]'>R</li>
+              <li className='mt-[-15px]'>O</li>
+              <li className='mt-[-15px]'>I</li>
+              <li className='mt-[-15px]'>N</li>
+              <li className='mt-[-15px]'>E</li>
+            </ul>
+          </div>
+          <div className='basis 2/3 [@media(max-width:400px)]:ml-[-100px] ml-[58px]'>
+            <img src="/elysia10.jpg" alt="" className='[@media(max-width:400px)]:static [@media(max-width:400px)]:w-[250px] [@media(max-width:400px)]:h-[320px] absolute border-[3px] w-[390px] h-[420px] rounded-[20px]'/>
+          </div>
+        </div>
+      </div>
+      <div className='basis-2/3 border-l-[2px] ml-[-60px] mr-[20px]'>
+        <div>
+          <img src="/elysia2.jpg" alt="" className='border-[3px] rounded-[20px] w-[734px] h-[413px] mt-[84px]' />
+        </div>
+        <div className=' ml-[45px] mt-[100px]'>
+          <h1 className='text-[64px]'>Fight For Humanity</h1>
+          <p className='text-[24px] leading-tight text-justify mr-[60px]'>Throughout her journey, Elysia makes the tragic decision to become the Herrscher of the End, aiming to create a significant threat that would compel humanity to unite. However, her actions are frequently misunderstood by her peers, including Kevin, the leader of the Flame-Chasers. Kevin believes that Elysia has become a danger and must be stopped for the sake of humanity&apos;s safety.</p>
+        </div>
+        <div className='mt-[30px] pb-[100px]'>
+          <ul className='flex space-x-[32px] space-y-[25px] justify-end'>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+          </ul>
+          <ul className='flex space-x-[32px] space-y-[25px] justify-end'>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+            <li className={`${styles.bgpink} w-[10px] h-[10px] border`}></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home2
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Home1 from "./components/Home1";
+import Home2 from "./components/Home2";
+import Home3 from "./components/Home3";
+import Home4 from "./components/Home4";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Home1 />
+      <Home2 />
+      <Home3 />
+      <Home4 />
+    </main>
+  );
+}
